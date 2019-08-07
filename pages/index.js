0f8eca6a07e3cb2f09fb1ca7fb183cecee220568
sara <sara@tablethotels.com>
2019-08-07 11:25:35 -0400

    Landing page animations

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,34 @@ import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
 // import Nav from '../components/nav'
+import { useState } from 'react';
 import './index.scss';
 
-const Home = () => (
-  <div>
-    <Head title="Dankport" />
-		<section className="Home">
-			<div className="Home__intro">
-				<h1 className="Home__title dp-title">Dankport.</h1>
-				<h2 className="Home__subtitle dp-subtitle">The shopping portal for all your herbal needs.</h2>
-			</div>
-		</section>
-  </div>
-)
+const Home = () => {
+	const [animationState, setAnimation] = useState('starting');
+
+	const startAnimation = () => {
+		setAnimation('done');
+	}
+
+	setTimeout(startAnimation);
+
+	return (
+		<div>
+			<Head title="Dankport" />
+			<section className={`Home ${animationState === 'starting' ? 'Home--start': 'Home--end'}`}>
+				<div className="Home__overlay">
+					<div className="Home__intro">
+						<h1 className="Home__title dp-title">Dankport</h1>
+						<h2 className="Home__subtitle dp-subtitle">The shopping portal for all your herbal needs.</h2>
+					</div>
+
+					<div className="Home__blurb dp-supporting-text">Currently under construction</div>
+				</div>
+			</section>
+
+		</div>
+	)
+}
 
 export default Home
