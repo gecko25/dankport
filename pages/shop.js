674d70c890149f407657f35e314c9aeaf567a821
sara <sara@tablethotels.com>
2019-08-08 23:56:57 -0400

    Adding cart summary to nav

--- pages/shop.js
+++ pages/shop.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Head from '../components/head'
 import { Nav, Product } from '../components/'
 import './index.scss';
+import '../styles/snip-override.scss';
+
 
 const Shop = ({ products }) => {
 	return (
